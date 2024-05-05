1bac217fba4c97fbc109fc58b8e9ca595e19a317
Erica Ospina <ericaospinaruiz@gmail.com>
2024-05-05 21:03:38 +0100

    Added new file helloWorld.py

---
+++ helloWorld.py
@@ -0,0 +1,4 @@
+# This task prints out the message Hello World
+# The code is now being uploaded to GitHub
+# Now the file is staged, the message is changed to "Git is awesome!"
+print("Git is awesome!")
